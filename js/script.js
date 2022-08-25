db0b0f9e90de6d57cf1ffd224ca24f72b51b6512
Vitaliy Demyanenko <padre.vitaliy@gmail.com>
2022-08-25 19:15:52 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+//
+// npx json-server db.json
+// npx webpack
+//babel
+
+
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import slider from './modules/slider';
+import openModal from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(()=> openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-09-01');
+    cards();
+    calc();
+    forms('form', modalTimerId);
+    slider({
+        slide: '.offer__slide',
+        container: '.offer__slider',
+        prevArrow: '.offer__slider-prev',
+        nextArrow: '.offer__slider-next',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+});
+
+
+function disemvowel(str) {
+
+
+    
+    return str;
+  }
+
+  console.log(disemvowel("QWERTY QWrer QWEsdf"));
